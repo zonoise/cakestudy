6b5fe0fc595cd1595adb8b22d2a48add1344dfb2
zonoise <zonoise@gmail.com>
2016-09-13 12:27:06 +0000

    add SubmissionInsertShell.php

--- src/Shell/SubmissionInsertShell.php
+++ src/Shell/SubmissionInsertShell.php
@@ -2,6 +2,9 @@
 namespace App\Shell;
 
 use Cake\Console\Shell;
+use \App\Model\Table\SubmissionsTable ;
+use Cake\Filesystem\File;
+use Cake\ORM\TableRegistry;
 
 /**
  * SubmissionInsert shell command.
@@ -19,7 +22,7 @@ class SubmissionInsertShell extends Shell
     public function getOptionParser()
     {
         $parser = parent::getOptionParser();
-
+        $parser->addArgument('filename', ['help' => 'CSV file path','required'=>true]);
         return $parser;
     }
 
@@ -30,6 +33,51 @@ class SubmissionInsertShell extends Shell
      */
     public function main() 
     {
+        $filename = $this->args[0];
+        $this->insert($filename);
+        
         $this->out($this->OptionParser->help());
     }
+    
+    private function insert($filename){
+        $handle = fopen($filename, "r");
+        $SubmissionsTable = TableRegistry::get('Submissions');
+
+        //$SubmissionsTable = new SubmissionsTable();
+        
+        //property_to_csv_column mapping
+        $mapping = ['contest_id' =>0,
+         'created' =>1,
+         'task_id' =>2,
+         'user_name' =>3, 
+         'langage' =>4,
+         'score' =>5,
+        //  'source_code_size' =>6
+        //  'status' =>7
+        //  'exec_time' =>8
+        //  'memory_usage' =>9
+          'url' =>10
+        ];
+         
+        
+        while(($data = fgetcsv($handle, 1000, ",")) !== FALSE){
+            $submission = $SubmissionsTable->newEntity();
+        
+            foreach($mapping as $property => $index){
+                $submission->{$property} = $data[$index];
+            }
+            //var_dump($submission);
+            $SubmissionsTable->save($submission);
+            
+            // $num = count($data);
+            // echo "<p> $num fields in line $row: <br /></p>\n";
+            // $row++;
+            // for ($c=0; $c < $num; $c++) {
+            //     echo $data[$c] . "<br />\n";
+            // }
+        }
+        fclose($handle);
+        
+    }
+    
 }
