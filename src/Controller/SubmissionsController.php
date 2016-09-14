1839057fc220a1cb998d563c72c94bd75616483d
zonoise <zonoise@gmail.com>
2016-09-14 06:53:08 +0000

    SubmissionsController.php index  using query string

--- src/Controller/SubmissionsController.php
+++ src/Controller/SubmissionsController.php
@@ -7,6 +7,7 @@ use App\Controller\AppController;
  * Submissions Controller
  *
  * @property \App\Model\Table\SubmissionsTable $Submissions
+ * @property \App\Model\Table\TagsTable $Tags
  */
 class SubmissionsController extends AppController
 {
@@ -18,7 +19,12 @@ class SubmissionsController extends AppController
      */
     public function index()
     {
-        $submissions = $this->paginate($this->Submissions);
+        $name = $this->request->query('user_name');
+        $q = $this->Submissions->find()->where(['user_name'=>$name ])->order(['created'=>'desc']);
+        $submissions = $this->paginate($q);
+
+
+        //$submissions = $this->paginate($this->Submissions);
 
         $this->set(compact('submissions'));
         $this->set('_serialize', ['submissions']);
@@ -41,71 +47,4 @@ class SubmissionsController extends AppController
         $this->set('_serialize', ['submission']);
     }
 
-    /**
-     * Add method
-     *
-     * @return \Cake\Network\Response|void Redirects on successful add, renders view otherwise.
-     */
-    public function add()
-    {
-        $submission = $this->Submissions->newEntity();
-        if ($this->request->is('post')) {
-            $submission = $this->Submissions->patchEntity($submission, $this->request->data);
-            if ($this->Submissions->save($submission)) {
-                $this->Flash->success(__('The submission has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
-            } else {
-                $this->Flash->error(__('The submission could not be saved. Please, try again.'));
-            }
-        }
-        $this->set(compact('submission'));
-        $this->set('_serialize', ['submission']);
-    }
-
-    /**
-     * Edit method
-     *
-     * @param string|null $id Submission id.
-     * @return \Cake\Network\Response|void Redirects on successful edit, renders view otherwise.
-     * @throws \Cake\Network\Exception\NotFoundException When record not found.
-     */
-    public function edit($id = null)
-    {
-        $submission = $this->Submissions->get($id, [
-            'contain' => []
-        ]);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $submission = $this->Submissions->patchEntity($submission, $this->request->data);
-            if ($this->Submissions->save($submission)) {
-                $this->Flash->success(__('The submission has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
-            } else {
-                $this->Flash->error(__('The submission could not be saved. Please, try again.'));
-            }
-        }
-        $this->set(compact('submission'));
-        $this->set('_serialize', ['submission']);
-    }
-
-    /**
-     * Delete method
-     *
-     * @param string|null $id Submission id.
-     * @return \Cake\Network\Response|null Redirects to index.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function delete($id = null)
-    {
-        $this->request->allowMethod(['post', 'delete']);
-        $submission = $this->Submissions->get($id);
-        if ($this->Submissions->delete($submission)) {
-            $this->Flash->success(__('The submission has been deleted.'));
-        } else {
-            $this->Flash->error(__('The submission could not be deleted. Please, try again.'));
-        }
-
-        return $this->redirect(['action' => 'index']);
-    }
 }
